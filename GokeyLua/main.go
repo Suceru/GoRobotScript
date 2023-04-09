8488d8be8a92f2c99d49de733f45614252bc4027
1246454212 <1246454212@qq.com>
2023-04-09 20:28:12 +0800

    修正GokeyLua支持读取脚本

--- GokeyLua/main.go
+++ GokeyLua/main.go
@@ -2,14 +2,54 @@ package main
 
 import (
 	CallGo "GoRobotScript/GokeyLua/Tool"
+	"flag"
+	"fmt"
 	lua "github.com/yuin/gopher-lua"
+	"os"
 )
 
+func ReadscriptUsage() {
+	HaveusageText := `ScriptPath is err`
+	fmt.Fprintf(os.Stderr, "%s\n\n", HaveusageText)
+
+}
 func main() {
 	L := lua.NewState()
 	defer L.Close()
 	L.PreloadModule("CallGo", CallGo.Loader)
-	if err := L.DoFile("GokeyLua/hello.lua"); err != nil {
-		panic(err)
+	scriptpath := flag.String("script", "path", "Script Path")
+	//scriptpatht := *scriptpath
+	Simkeyrun := "main"
+	flag.Parse()
+	if len(os.Args) == 1 {
+		flag.Usage = ReadscriptUsage
+		flag.Usage()
+		fmt.Fprintf(os.Stderr, "errargs")
+		return
 	}
+	switch os.Args[1] {
+	// case "dokey":
+
+	// 	s := Keyboard.String("cmd", "", "传入参数")
+	// 	Keyboard.Parse(os.Args[2:])
+	// 	for index, val := range *s {
+
+	// 		fmt.Println(index, string(val))
+	// 	}
+	// 	FKeyboard(*s)
+	case Simkeyrun:
+		if err := L.DoFile("GokeyLua/main.lua"); err != nil {
+			panic(err)
+		}
+	default:
+		if *scriptpath == "path" {
+			if err := L.DoFile(os.Args[1]); err != nil {
+				panic(err)
+			}
+		} else {
+			fmt.Fprintf(os.Stderr, "value=%s", *scriptpath)
+
+		}
+	}
+
 }
